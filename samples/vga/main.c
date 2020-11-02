a50cc573f91914e1e42e7e7c80f16bd8a141c2bd
Dhole <dhole@riseup.net>
2020-11-02 01:54:12 +0100

    Use GD32VF103_Firmware_Library

--- samples/vga/main.c
+++ samples/vga/main.c
@@ -24,6 +24,7 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY
  * OF SUCH DAMAGE.
  */
+#include "gd32vf103/mtimer.h"
 #include "lib/mtimer.h"
 #include "lib/eclic.h"
 #include "lib/rcu.h"
